7795746b187413033c87fe1a70c3624d19d007aa
Christopher Williams <cw@chriswillia.ms>
2021-09-06 12:25:07 -0500

    Updates allowed origins

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || ['https://mms-photo-board.cwilliams.now.sh', 'http://localhost:3000', /(192\.168\.1\.)(([0-2][0-9]){0}([0-9])){1,3}/],
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || ['https://mms-photo-board.cwilliams.vercel.app', 'https://mms-photo-board.cwilliams.now.sh', 'http://localhost:3000', /(192\.168\.1\.)(([0-2][0-9]){0}([0-9])){1,3}/],
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DATABASE_URL || process.env.LOCAL_DB_URL,
     JWT_SECRET: process.env.JWT_SECRET,
